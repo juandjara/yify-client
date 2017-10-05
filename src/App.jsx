3e2f3c7a564924801bf98a3043a42b714097b14f
Juan D. Jara <juanorigami@gmail.com>
2017-10-05 21:21:55 +0200

    feat: extract header for making search form work

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,11 @@
 import React, { Component } from 'react';
 import styled from 'styled-components'
 import { Route, Switch, BrowserRouter } from 'react-router-dom'
-import Link from './components/shared/LinkReset'
+import Header from './components/shared/Header'
 
 import MovieList from './components/routed/MovieList'
 import NotFound from './components/routed/NotFound'
 
-const Header = styled.header`
-  background: black;
-  color: white;
-  padding: 0.5em;
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: space-between;
-  align-items: center;
-`
-const H1 = styled.h1`
-  font-weight: 500;
-  margin: 0;
-  color: white;
-  font-size: 2em;
-  & .material-icons {
-    vertical-align: middle;
-    margin-left: 5px;
-    margin-bottom: 5px;
-  }
-`
-const Input = styled.input`
-  background: white;
-  padding: .5em;
-  border: none;
-  font-size: 1rem;
-`
-const SearchForm = styled.form`
-  display: flex;
-  & .material-icons {
-    padding: 5px;
-  }
-`
 const Root = styled.div`
   background: #2c3e50;
   color: white;
@@ -45,49 +13,16 @@ const Root = styled.div`
   padding-bottom: 2em;
 `
 
-class App extends Component {
-  state = {query: ""}
-  handleChange = ev => {
-    const {value} = ev.target
-    this.setState({query: value})
-  }
-  handleSubmit = ev => {
-    ev.preventDefault()
-  }
-  render() {
-    return (
-      <BrowserRouter> 
-        <Root>
-          <Header>
-            <H1>
-              <Link to="/">Palomovies</Link>
-              <small>
-                <a href="https://palomitas-dl.fuken.xyz"
-                   rel="noopener noreferrer"
-                   target="_blank"
-                   style={{color: 'white'}}>
-                  <i className="material-icons">local_movies</i>
-                </a>
-              </small>
-            </H1>
-            <SearchForm onSubmit={this.handleSubmit}>
-              <i title="" className="material-icons">search</i>
-              <Input
-                placeholder="Escribe para buscar pelis"
-                value={this.state.query}
-                onChange={this.handleChange}
-              />
-              <input type="submit" hidden />
-            </SearchForm>
-          </Header>
-          <Switch>
-            <Route path="/" exact component={MovieList} />
-            <Route component={NotFound} />
-          </Switch>
-        </Root>
-      </BrowserRouter>
-    );
-  }
-}
+const App = () => (
+  <BrowserRouter> 
+    <Root>
+      <Header />
+      <Switch>
+        <Route path="/" exact component={MovieList} />
+        <Route component={NotFound} />
+      </Switch>
+    </Root>
+  </BrowserRouter>
+)
 
 export default App;
---
+++ src/components/shared/Header.jsx
@@ -0,0 +1,84 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import styled from 'styled-components'
+import Link from './LinkReset'
+import Icon from './Icon'
+
+const StyledHeader = styled.header`
+  background: black;
+  color: white;
+  padding: 0.5em;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  align-items: center;
+`
+const H1 = styled.h1`
+  font-weight: 500;
+  margin: 0;
+  color: white;
+  font-size: 2em;
+  & .material-icons {
+    vertical-align: middle;
+    margin-left: 5px;
+    margin-bottom: 5px;
+  }
+`
+const Input = styled.input`
+  background: white;
+  padding: .5em;
+  border: none;
+  font-size: 1rem;
+  color: #333;
+`
+const SearchForm = styled.form`
+  display: flex;
+  & .material-icons {
+    padding: 5px;
+  }
+`
+
+export default class Header extends Component {
+  state = {
+    query: ""
+  }
+  static contextTypes = {
+    router: PropTypes.object
+  }
+  handleSubmit = ev => {
+    ev.preventDefault()
+    const {query}   = this.state
+    const {history} = this.context.router
+    history.push(`/?q=${query}`)
+  }
+  handleChange = ev => {
+    const {value} = ev.target
+    this.setState({query: value})
+  }
+  render () {
+    return (
+      <StyledHeader>
+        <H1>
+          <Link to="/">Palomovies</Link>
+          <small>
+            <a href="https://palomitas-dl.fuken.xyz"
+              rel="noopener noreferrer"
+              target="_blank"
+              style={{color: 'white'}}>
+              <Icon>local_movies</Icon>
+            </a>
+          </small>
+        </H1>
+        <SearchForm onSubmit={this.handleSubmit}>
+          <Icon>search</Icon>
+          <Input
+            placeholder="Escribe para buscar pelis"
+            value={this.state.query}
+            onChange={this.handleChange}
+          />
+          <input type="submit" hidden />
+        </SearchForm>
+      </StyledHeader>
+    );
+  }
+}
